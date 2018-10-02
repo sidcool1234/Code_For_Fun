f6e5903adab24ec70cfe529d2f7dedf66787f12b
Siddharth Kulkarni <sidcool1234@gmail.com>
2018-10-02 20:44:53 +0530

    Problem solution for Arrays - Better solution

--- problem_set_two/problems.py
+++ problem_set_two/problems.py
@@ -17,6 +17,13 @@ class Solution:
 
     return count
 
+  # Better Solution
+  def another_cover_pair(self, source, target):
+    #return max(abs(point_from[0] - point_to[0], point_from[1] - point_to[1]))
+    return max(abs(source[0]-target[0]), abs(source[1]-target[1]))
+
+
+  # Bad Solution
   def cover_pair(self, source, target):
     count = 0
     x_from = source[0]
@@ -56,12 +63,20 @@ class Solution:
     return count
 
 
-# def test_cover_pair():
-#   x = Solution().cover_pair([0,1], [4,3])
-#   print(x)
+def test_cover_pair():
+  print(Solution().cover_pair([0,1], [4,3]))
+  print(Solution().cover_pair([-7, -13], [1, -5]))
+
 #
 # def test_cover_points():
 #   x = Solution().cover_points([-7, -13], [1, -5])
 #   print(x)
 #
 # test_cover_points()
+
+def test_another_cover_pair():
+  print(Solution().another_cover_pair([0,1], [4,3]))
+  print(Solution().another_cover_pair([-7, -13], [1, -5]))
+
+test_cover_pair()
+test_another_cover_pair()
