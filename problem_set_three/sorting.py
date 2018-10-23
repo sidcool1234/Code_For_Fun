f193adf09a864368bb7e81b1ae8df19851f75711
Siddharth Kulkarni <sidcool1234@gmail.com>
2018-10-23 22:04:32 +0530

    Insertion sort

--- problem_set_three/sorting.py
+++ problem_set_three/sorting.py
@@ -1,18 +1,19 @@
 import random
 
+
 def insertion_sort_inplace(array):
-  for index, value in enumerate(array):
-    if index == 0:
-      continue
+    for index, value in enumerate(array):
+        if index == 0:
+            continue
 
-    i = index - 1
-    while i>=0 and array[i] > value:
-      array[i+1] = array[i]
-      i = i - 1
+        i = index - 1
+        while i >= 0 and array[i] > value:
+            array[i + 1] = array[i]
+            array[i] = value
+            i = i - 1
 
-    array[i + 1] = value
+    return array
 
-  return array
-
-my_randoms = [random.randrange(1, 101, 1) for _ in range(20)]
+my_randoms = [random.randrange(1, 100, 1) for _ in range(5)]
+print("input = ", my_randoms)
 print(insertion_sort_inplace(my_randoms))
